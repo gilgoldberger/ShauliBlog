65e4b77d8f57b3df4f2ac7f87379a0e056f1c3eb
gilgoldberger <gilgoldberger@gmail.com>
2018-09-15 18:17:00 +0200

    adding app and modles

---
+++ ShauliBlog/Models/CommentCityView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ClassExe3.Models
+{
+    public class CommentCityView
+    {
+        [DisplayName("Number of comments")]
+        public int numOfComments { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ShauliBlog/Models/LocationView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassExe3.Models
+{
+    public class LocationView
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
